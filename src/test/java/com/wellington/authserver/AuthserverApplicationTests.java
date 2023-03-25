d6b5d12f4111234489d9015c85e1f799cb207a3e
WellingtonJr <wferreira.bm13@gmail.com>
2023-03-25 11:54:08 -0300

    first commit

---
+++ src/test/java/com/wellington/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellington.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
