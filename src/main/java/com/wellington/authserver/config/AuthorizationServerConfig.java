bb4b182fa79efd499f0be8d9d3539cd2c3d34df0
Wellington Junior <Wellington Junior>
2023-03-27 09:33:00 -0300

    22.19. Testando o fluxo Authorization Code com um client JavaScript

--- src/main/java/com/wellington/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/wellington/authserver/config/AuthorizationServerConfig.java
@@ -47,11 +47,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret(passwordEncoder.encode("outra2123"))
                 .authorizedGrantTypes("authorization_code")
                 .scopes("write", "read")
-                .redirectUris("http://aplicacao-cliente")
+                .redirectUris("http://127.0.0.1:5500")
                 // CLIENT DO RESOURCE SERVER
                 .and()
                 .withClient("checkToken")
-                .secret("check123");
+                .secret(passwordEncoder.encode("check123"));
     }
 
     @Override
