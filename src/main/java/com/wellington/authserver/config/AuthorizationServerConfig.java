72b85c215a4ad8a08744b2f04154a4f48cf64e98
WellingtonJr <wferreira.bm13@gmail.com>
2023-03-30 00:37:24 -0300

    23.9. Assinando o JWT com RSA SHA-256 (chave assimétrica)

--- src/main/java/com/wellington/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/wellington/authserver/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,6 +19,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import com.wellington.authserver.security.PkceAuthorizationCodeTokenGranter;
@@ -101,7 +103,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("SAFDASDFASDASDGASDFASDFASDF2FSADFASDFVASVASDF2FASDFASF");
+        // jwtAccessTokenConverter.setSigningKey("SAFDASDFASDASDGASDFASDFASDF2FSADFASDFVASVASDF2FASDFASF");
+        var jksResource = new ClassPathResource("/keystores/courseapi.jks");
+        var keyStorePass = "123456";
+        var keyPairAlias = "courseapi";
+
+        var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+        var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+
+        jwtAccessTokenConverter.setKeyPair(keyPair);
 
         return jwtAccessTokenConverter;
     }
