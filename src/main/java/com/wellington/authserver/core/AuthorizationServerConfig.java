1a5a710d662f4131bc289dcd12ddc92bc88a96f1
WellingtonJr <wferreira.bm13@gmail.com>
2023-04-06 13:11:28 -0300

    27.14. Customizando o token JWT com dados do usuário

--- src/main/java/com/wellington/authserver/core/AuthorizationServerConfig.java
+++ src/main/java/com/wellington/authserver/core/AuthorizationServerConfig.java
@@ -4,6 +4,8 @@ import java.io.InputStream;
 import java.security.KeyStore;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -27,6 +32,8 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -34,6 +41,8 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.wellington.authserver.domain.Usuario;
+import com.wellington.authserver.domain.UsuarioRepository;
 
 @Configuration
 public class AuthorizationServerConfig {
@@ -144,6 +153,27 @@ public class AuthorizationServerConfig {
 
                 RSAKey rsaKey = RSAKey.load(keyStore, keypairAlias, keyStorePass);
                 return new ImmutableJWKSet<>(new JWKSet(rsaKey));
+        }
+
+        @Bean
+        public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer(UsuarioRepository usuarioRepository) {
+                return context -> {
+                        Authentication authentication = context.getPrincipal();
+                        if (authentication.getPrincipal() instanceof User) {
+                                User user = (User) authentication.getPrincipal();
+
+                                Usuario usuario = usuarioRepository.findByEmail(user.getUsername()).orElseThrow();
+                                Set<String> authorities = new HashSet<String>();
+
+                                for (GrantedAuthority authority : user.getAuthorities()) {
+                                        authorities.add(authority.getAuthority());
+                                }
+
+                                context.getClaims().claim("usuario_id", usuario.getUsuarioId().toString());
+                                context.getClaims().claim("authorities", authorities);
+
+                        }
+                };
         }
 
 }
